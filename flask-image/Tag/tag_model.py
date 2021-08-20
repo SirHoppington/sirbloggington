d56e8de7180026a7ddd9699425a43c918cd85c5c
SirHoppington <nickhopgood@gmail.com>
2021-08-20 09:20:00 +0100

    dev update

---
+++ flask-image/Tag/tag_model.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    @property
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+        }
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
